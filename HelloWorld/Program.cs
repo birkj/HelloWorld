64a4ba32b5f2bc39a26dfddbf820dd61eaeffe3e
jia <jbia@itu.dk>
2021-09-03 14:26:16 +0200

    Tested and implemented IsLeapYear

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,11 +2,24 @@
 
 namespace HelloWorld
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Jens!!");
+        }
+
+        public bool isLeapYear(int year) {
+            
+            if(year % 100 == 0 && year % 400 == 0){
+                return true;
+            } else if (year % 100 == 0) {
+                return false;
+            } else if(year % 4 == 0) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 }
